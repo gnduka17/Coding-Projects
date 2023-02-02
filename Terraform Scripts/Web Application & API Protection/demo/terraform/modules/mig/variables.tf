84073936c9078d6390cf658ea30b0129b9f37440
Gloria Nduka <nduka@usc.edu>
2023-02-01 17:03:54 -0800

    Add files via upload

--- Terraform Scripts/Web Application & API Protection/demo/terraform/modules/mig/variables.tf
+++ Terraform Scripts/Web Application & API Protection/demo/terraform/modules/mig/variables.tf
@@ -1,1 +1,14 @@
-
+variable "project_id" {}
+variable "vpc_id" {}
+variable "subnet_id" {}
+variable "primary-region" {}
+variable "apigee_hostname" {}
+variable "l4-ip" {}
+variable "cloud_armour_policy" {}
+variable "gce_service_account_roles" {
+    default     = [
+        "compute.instanceAdmin.v1",
+        "storage.objectViewer",
+        "compute.networkAdmin"
+    ]
+}
